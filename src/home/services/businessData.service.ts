210d0be01b1c2808d654afdda91031968210e1a6
Mohcine A. H <104150529+mabdeh@users.noreply.github.com>
2022-05-01 18:17:57 +0200

    Question 1 of the Zefir test
    - for the first function (computeNegociationMargin) : I proposed a simple call to the math.min function of Javascript to reach the searched result. 
    - for the second function (computeServiceFees) : I proposed in a first stage an "if" loop through the 6 strips to which the finalOfferPrice may belongs to. And then through the department where the home is. NB : Is the rate of a home in the sixth strip in Lyon or Nantes actual ? I read 99.9999%...

--- src/home/services/businessData.service.ts
+++ src/home/services/businessData.service.ts
@@ -12,6 +12,50 @@ export default class BusinessDataService {
     private readonly businessDataRepository: BusinessDataCustomRepository,
     private readonly homeService: HomeService
   ) {}
+  
+  // QUESTION 1.
+  // First part : the computation of the negociation margin function
+  async computeNegociationMargin(
+        finalOfferPrice: number,
+        targetSalePrice: number,
+        maxNegociationMargin: number
+    ): Promise<number> {
+        return Math.min(targetSalePrice / finalOfferPrice - 1, maxNegociationMargin)
+    }
+  // Second part : the computation of the service fees of an offer
+  async computeServiceFees(finalOfferPrice: number, zipCode: number): Promise<number> {
+        let dept = Math.round(zipCode * 0.001)          // Obtain department code from the zip code
+        if (finalOfferPrice < 100 000) {                // Strip 1 : under 100k
+            if (dept === 59) { return 15 000 }
+            elif(dept === 44 || dept === 69){ return 20 000 }
+            elif(dept in [75, 92, 93, 94]){ return 20 000 }
+        }
+        elif(finalOfferPrice < 145 000 && finalOfferPrice >= 100 000){      // Strip 2 : over 100k but under 145k
+            if (dept === 59) { return 19 000 }
+            elif(dept === 44 || dept === 69){ return 22 000 }
+            elif(dept in [75, 92, 93, 94]){ return 22 000 }
+        }
+        elif(finalOfferPrice < 200 000 && finalOfferPrice >= 145 000){      // Strip 3 : over 145k but under 200k
+            if (dept === 59) { return 20 000 }
+            elif(dept === 44 || dept === 69){ return 23 000 }
+            elif(dept in [75, 92, 93, 94]){ return 23 000 }
+        }
+        elif(finalOfferPrice < 400 000 && finalOfferPrice >= 200 000){      // Strip 4 : over 200k but under 400k
+            if (dept === 59) { return 0.1 * finalOfferPrice }
+            elif(dept === 44 || dept === 69){ return 0.11 * finalOfferPrice }
+            elif(dept in [75, 92, 93, 94]){ return 0.11 * finalOfferPrice }
+        }
+        elif(finalOfferPrice < 650 000 && finalOfferPrice >= 400 000){      // Strip 5 : over 400k but under 650k
+            if (dept === 59) { return 0.08 * finalOfferPrice }
+            elif(dept === 44 || dept === 69){ return 0.08 * finalOfferPrice }
+            elif(dept in [75, 92, 93, 94]){ return 0.08 * finalOfferPrice }
+        }
+   else {                                                                   // Strip 6 : over 650k
+            if (dept === 59) { return 0.3 * finalOfferPrice }
+            elif(dept === 44 || dept === 69){ return 0.999999 * finalOfferPrice }       // Is this rate actual ? To ask the interviewers
+            elif(dept in [75, 92, 93, 94]){ return 0.1 * finalOfferPrice }
+        }
+    }
 
   async generateBusinessDataForHome(
     homeUuid: string,
